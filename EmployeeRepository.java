deb1986fb099ab22eeb75af64fc53d9e36953b87
Andrii Shauro <123452950+shurikandrew@users.noreply.github.com>
2023-11-20 11:15:14 +0100

    Deleted repeated .java file

--- EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shurikandrew.CRUDdemo.dao;
-
-import com.shurikandrew.CRUDdemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
