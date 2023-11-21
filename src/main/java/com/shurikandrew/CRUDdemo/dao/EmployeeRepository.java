2d54a8bdd6e0049df18f81f1f20ed948d091009f
Andrii Shauro <123452950+shurikandrew@users.noreply.github.com>
2023-11-21 09:17:59 +0100

    Delete src/main/java/com/shurikandrew/CRUDdemo/dao/EmployeeRepository.java

--- src/main/java/com/shurikandrew/CRUDdemo/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shurikandrew.CRUDdemo.dao;
-
-import com.shurikandrew.CRUDdemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "workers")
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
