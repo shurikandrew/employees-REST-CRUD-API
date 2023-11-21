ae51f87f8c30579cb2929dda2fefe3183353f1f1
Andrii Shauro <123452950+shurikandrew@users.noreply.github.com>
2023-11-21 09:11:27 +0100

    Add files via upload

--- src/main/java/com/shurikandrew/CRUDdemo/dao/EmployeeRepository.java
+++ src/main/java/com/shurikandrew/CRUDdemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.shurikandrew.CRUDdemo.dao;
 
 import com.shurikandrew.CRUDdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "workers")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
